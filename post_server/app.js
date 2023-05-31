e7d8a2841acc98990508d0178f203b94cbc3fe4e
vijay9897 <vijay.kumar@droom.in>
2023-05-31 20:44:10 +0530

    front end project setup and database connection for server done

--- post_server/app.js
+++ post_server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -16,4 +17,11 @@ app.use((req, res, next) => {
 
 app.use('/feed', feedRoutes);
 
-app.listen(8081);
+mongoose.connect(
+    'mongodb://127.0.0.1:27017/posts_db'
+).then(result => {
+    console.log('Connect to database');
+    app.listen(8081);
+}).catch(err => {
+    console.log(err);
+});
--- post_server/controllers/feed.js
+++ post_server/controllers/feed.js
@@ -1,23 +1,48 @@
+const { validationResult } = require('express-validator');
+
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [
             {
+                _id: '343',
                 title: 'First Post',
-                content: 'This is first post'
+                content: 'This is first post',
+                imageUrl: 'images/sample.png',
+                creator: {
+                    name: 'vijay'
+                },
+                createdAt: new Date()
             }
         ]
     });
 };
 
 exports.createPost = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            message: 'Validation failed, entered data is incorrect.',
+            errors: errors.array()
+        });
+    }
     const title = req.body.title;
     const content = req.body.content;
-    res.status(201).json({
-        message: 'Post created!',
-        post: {
-            id: new Date().toISOString(),
-            title: title,
-            content: content
-        }
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: 'images/sample.png',
+        creator: {
+            name: 'Vijay'
+        },
+    });
+    post.save().then(result => {
+        res.status(201).json({
+            message: 'Post created!',
+            post: result
+        });
+    }).catch(err => {
+        console.log(err);
     });
 };
