5c4d303b94706ef39ff1596f188ef01ebed92e44
vijay9897 <vijay.kumar@droom.in>
2023-05-31 23:46:23 +0530

    single post api created

--- post_server/app.js
+++ post_server/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
@@ -7,6 +9,7 @@ const feedRoutes = require('./routes/feed');
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,6 +19,15 @@ app.use((req, res, next) => {
 });
 
 app.use('/feed', feedRoutes);
+
+app.use((error, req, res, next) => {
+    console.log(error);
+    const status = error.statusCode || 500;
+    const message = error.message;
+    res.status(status).json({
+        message: message
+    });
+});
 
 mongoose.connect(
     'mongodb://127.0.0.1:27017/posts_db'
--- post_server/controllers/feed.js
+++ post_server/controllers/feed.js
@@ -3,29 +3,26 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-    res.status(200).json({
-        posts: [
-            {
-                _id: '343',
-                title: 'First Post',
-                content: 'This is first post',
-                imageUrl: 'images/sample.png',
-                creator: {
-                    name: 'vijay'
-                },
-                createdAt: new Date()
-            }
-        ]
-    });
+    Post.find()
+    .then(posts => {
+        res.status(200).json({
+            message: 'Posts fetched',
+            posts: posts
+        });
+    }).catch(err => {
+        if(!err.statusCode) {
+            err.statusCode = 500
+        }
+        next(err);
+    })
 };
 
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(422).json({
-            message: 'Validation failed, entered data is incorrect.',
-            errors: errors.array()
-        });
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
     }
     const title = req.body.title;
     const content = req.body.content;
@@ -43,6 +40,31 @@ exports.createPost = (req, res, next) => {
             post: result
         });
     }).catch(err => {
-        console.log(err);
+        if(!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
+    });
+};
+
+exports.getPost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+    .then(post => {
+        if (!post) {
+            const error = new Error('Could not found post');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({
+            message: 'Post fetched',
+            post: post
+        });
+    })
+    .catch(err => {
+        if(!err.statusCode) {
+            err.statusCode = 500
+        }
+        next(err);
     });
 };
